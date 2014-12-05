f4e312a5bbc88e517f0fe37d329ba3e24fc07f73
Jakob Harlan <jakob.harlan@uni-weimar.de>
2014-12-05 17:44:55 +0100

    +hit r to reload json, +hdr options

--- jsonloader.py
+++ jsonloader.py
@@ -107,8 +107,13 @@ class jsonloader:
     background_color = self.json_data["pipeline_options"]["background_settings"]["color"]
     pipe.BackgroundColor.value   = avango.gua.Color(background_color[0], background_color[1], background_color[2])
 
+    # VIGNETTE
     pipe.EnableVignette.value   = self.json_data["pipeline_options"]["vignette_settings"]["enable"]
     vignette_color = self.json_data["pipeline_options"]["vignette_settings"]["color"]
     pipe.VignetteColor.value   = avango.gua.Color(vignette_color[0], vignette_color[1], vignette_color[2])
     pipe.VignetteCoverage.value = self.json_data["pipeline_options"]["vignette_settings"]["coverage"]
     pipe.VignetteSoftness.value = self.json_data["pipeline_options"]["vignette_settings"]["softness"]
+
+    # HDR
+    pipe.EnableHDR.value = self.json_data["pipeline_options"]["hdr_settings"]["enable"]
+    pipe.HDRKey.value    = self.json_data["pipeline_options"]["hdr_settings"]["key"]
--- main.py
+++ main.py
@@ -2,19 +2,54 @@
 
 import avango
 import avango.script
+from examples_common.GuaVE import GuaVE
+from avango.script import field_has_changed
 import avango.gua
 
-from examples_common.GuaVE import GuaVE
+from examples_common import device
 
 import jsonloader
+
+
+class Reloader(avango.script.Script):
+
+  Keyboard = device.KeyboardDevice()
+
+
+  def __init__(self):
+    self.super(Reloader).__init__()
+    self.always_evaluate(True)
+
+    self.KeyR = False
+
+
+  def myConstructor(self, json_path, root, pipe):
+    self.loader = jsonloader.jsonloader()
+    self.json_path = json_path
+    self.root = root
+    self.pipe = pipe
+    
+    self.loader.load_json("test.json", self.root)
+    self.loader.load_and_set_PipelineOptions(self.pipe)
+
+
+  def evaluate(self):
+    # Key P for printing Cone Tree
+    if self.Keyboard.KeyR.value and not self.KeyR:
+      self.root.Children.value = []
+      self.loader.load_json("test.json", self.root)
+      self.loader.load_and_set_PipelineOptions(self.pipe)
+    self.KeyR = self.Keyboard.KeyR.value
+
+
 
 def start():
 
   # setup scenegraph
   graph = avango.gua.nodes.SceneGraph(Name = "scenegraph")
 
-  loader = jsonloader.jsonloader()
-  loader.load_json("test.json", graph.Root.value)
+  scene = avango.gua.nodes.TransformNode(Name = "scene")
+  scene.Transform.value = avango.gua.make_identity_mat()
 
   eye = avango.gua.nodes.TransformNode(Name = "eye")
   eye.Transform.value = avango.gua.make_trans_mat(0.0, 0.0, 3.5)
@@ -22,7 +57,7 @@ def start():
   screen = avango.gua.nodes.ScreenNode(Name = "screen", Width = 4, Height = 3)
   screen.Children.value = [eye]
 
-  graph.Root.value.Children.value.append(screen)
+  graph.Root.value.Children.value = [screen, scene]
 
   # setup viewing
   size = avango.gua.Vec2ui(1024, 768)
@@ -35,8 +70,9 @@ def start():
                                                                     LeftResolution = size),
                                    LeftResolution = size)
 
-  loader.load_and_set_PipelineOptions(pipe)
 
+  reloader = Reloader()
+  reloader.myConstructor("test.json", scene, pipe)
 
   #setup viewer
   viewer = avango.gua.nodes.Viewer()
